1ccdacedc5fffe9c0eee2c084929075f8151304c
Johan Heman <jojo2001@hotmail.se>
2026-04-14 09:02:59 +0200

    setup fetch

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const input = document.getElementById("input")
 const addBtn = document.getElementById("add-btn")
 let ingredientsList = document.getElementById("ingredient-list")
 
+let ingredients = []
+
+const foodURL = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel"
+
+
+let nutritionURL = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel/{nummer}/naringsvarden"
 
 
 
+
+fetch(foodURL)
+.then(response => response.json())
+.then(function (data) {
+    ingredients = data.livsmedel
+    console.log(ingredients)
+})
 
