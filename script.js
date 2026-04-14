288504481c5a684c4478f459116c0d99fadfa345
Johan Heman <jojo2001@hotmail.se>
2026-04-14 09:10:38 +0200

    Setup fetch for foodURL

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ let nutritionURL = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livs
 fetch(foodURL)
 .then(response => response.json())
 .then(function (data) {
-    ingredients = data.livsmedel
+    ingredients = data.livsmedel.map(function (item){
+        return {
+            id: item.nummer,
+            name: item.namn,
+            type: item.livsmedelsTyp
+        }
+    })
     console.log(ingredients)
 })
 
